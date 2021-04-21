eb62399f0baaa61ee40f87e52333447e2e9e6170
mmona-16 <mmehra04@outlook.com>
2021-04-21 21:31:40 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,20 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('client'))
+function getNLUInstance() {
+let api_key = process.env.API_KEY;
+let api_url = process.env.API_URL;
 
-const cors_app = require('cors');
-app.use(cors_app());
+const NaturalLanguageUnderstandingV1 = require(‘ibm-watson/natural-language-understanding/v1’);
+const { IamAuthenticator } = require(‘ibm-watson/auth’);
 
-app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+ version: '2020-08-01',
+  authenticator: new IamAuthenticator({
+   apikey:api_key,
+  }),
+  serviceUrl: api_url,
 });
-
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
-});
-
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
-});
-
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
-});
-
-let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
-
+return naturalLanguageUnderstanding;
+}
